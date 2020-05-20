3e42aa1a02a8b5843c673a9bfffc8ca77953d4b2
Julien Bernard <julien.bernard@univ-fcomte.fr>
2020-05-20 16:33:51 +0200

    add code

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # cleancode
+
+A tool to remove good answers from code and replace them with stubs.
+
+## Example
+
+```cpp
+int factorial(int n) {
+  // <<<<
+  if (n <= 1) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
+  #if 0 // ====
+  return 0
+  #endif // >>>>
+}
+```
+
+After `cleancode`:
+
+```cpp
+int factorial(int n) {
+  return 0
+}
+```
---
+++ cleancode.cc
@@ -0,0 +1,58 @@
+#include <cassert>
+#include <iostream>
+#include <string>
+
+namespace {
+
+  bool hasTag(const std::string& line, const std::string& tag) {
+    return line.find(tag) != std::string::npos;
+  }
+
+  enum class State {
+    Outside,
+    InsideRemove,
+    InsideKeep,
+  };
+
+}
+
+int main() {
+  using namespace std::string_literals;
+
+  std::string line;
+  std::size_t count = 0;
+  bool keep = true;
+  State state = State::Outside;
+
+  for (std::string line; std::getline(std::cin, line); ) {
+    ++count;
+
+    if (hasTag(line, "<<<<"s)) {
+      if (state != State::Outside) {
+        std::cerr << "An opening tag was wrongly detected on line " << count << '\n';
+      }
+
+      state = State::InsideRemove;
+      keep = false;
+    } else if (hasTag(line, ">>>>"s)) {
+      if (state != State::InsideRemove && state != State::InsideKeep) {
+        std::cerr << "A closing tag was wrongly detected on line " << count << '\n';
+      }
+
+      state = State::Outside;
+      keep = true;
+    } else if (hasTag(line, "====")) {
+      if (state != State::InsideRemove) {
+        std::cerr << "A middle tag was wrongly detected on line " << count << '\n';
+      }
+
+      state = State::InsideKeep;
+      keep = true;
+    } else if (keep) {
+      assert(state == State::Outside);
+      std::cout << line << '\n';
+    }
+  }
+
+  return 0;
+}
